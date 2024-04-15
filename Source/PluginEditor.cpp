76dfa406e67c133fca6228ab8e4d474fef533c5a
Ritij Jutur <ritij.jutur@gmail.com>
2024-04-15 02:39:13 -0400

    implemented skeleton for custom slider visuals

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -129,6 +129,13 @@ void ResponseCurveComponent::paint (juce::Graphics& g)
 //==============================================================================
 ParrotAudioProcessorEditor::ParrotAudioProcessorEditor (ParrotAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p),
+peakFreqSlider(*audioProcessor.apvts.getParameter("Peak Freq"), "Hz"),
+peakGainSlider(*audioProcessor.apvts.getParameter("Peak Gain"), "dB"),
+peakQualitySlider(*audioProcessor.apvts.getParameter("Peak Quality"), ""),
+lowCutFreqSlider(*audioProcessor.apvts.getParameter("LowCut Freq"), "Hz"),
+highCutFreqSlider(*audioProcessor.apvts.getParameter("HighCut Freq"), "Hz"),
+lowCutSlopeSlider(*audioProcessor.apvts.getParameter("LowCut Slope"), "dB/Oct"),
+highCutSlopeSlider(*audioProcessor.apvts.getParameter("HighCut Slope"), "db/Oct"),
 responseCurveComponent(audioProcessor),
 peakFreqSliderAttachment(audioProcessor.apvts, "Peak Freq", peakFreqSlider),
 peakGainSliderAttachment(audioProcessor.apvts, "Peak Gain", peakGainSlider),
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,13 +11,41 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-struct CustomRotarySlider : juce::Slider
+struct LookAndFeel : juce::LookAndFeel_V4
 {
-    CustomRotarySlider() : juce::Slider (juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
-                                     juce::Slider::TextEntryBoxPosition::NoTextBox)
+    void drawRotarySlider(juce::Graphics&,
+                            int x, int y, int width, int height,
+                            float sliderPosProportional,
+                            float rotaryStartAngle,
+                            float rotaryEndAngle,
+                          juce::Slider&) override{ }
+};
+
+struct RotarySliderWithLabels : juce::Slider
+{
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) :
+    juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                 juce::Slider::TextEntryBoxPosition::NoTextBox),
+    param (&rap) ,
+    suffix(unitSuffix)
     {
-    
+        setLookAndFeel(&lnf);
     }
+    
+    ~RotarySliderWithLabels(){
+        setLookAndFeel(nullptr);
+    }
+    
+    void paint(juce::Graphics &g) override { }
+    juce::Rectangle<int> getSliderBounds() const;
+    int getTextHeight() const {return 14;}
+    juce::String getDisplayString() const;
+    
+private:
+    LookAndFeel lnf;
+    
+    juce::RangedAudioParameter* param;
+    juce::String suffix;
 };
 
 struct ResponseCurveComponent : juce::Component,
@@ -61,7 +89,7 @@ private:
     // access the processor object that created it.
     ParrotAudioProcessor& audioProcessor;
     
-    CustomRotarySlider peakFreqSlider,
+    RotarySliderWithLabels peakFreqSlider,
     peakGainSlider,
     peakQualitySlider,
     lowCutFreqSlider,
